76c6afefef741be5faec2e380f247cf6c6baa3be
ameyk <ameykhale@gmail.com>
2019-09-14 18:21:13 +0530

    Testing polling on Azure

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -235,5 +235,5 @@ def main():
 
 
 if __name__ == '__main__':
-    DEBUG = False       #When testing locally, change this to True
+    DEBUG = True       #When testing locally, change this to True.
     main()
